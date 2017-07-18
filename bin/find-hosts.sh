8353c7bc916f444bf6e07bd00b483c493d0af315
Dick Tang <dick.tang@ymail.com>
2017-07-19 01:30:06 +0800

    check ttl and do generate at find-hosts

--- bin/find-hosts.sh
+++ bin/find-hosts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 CACHE_DIR="$HOME/.jumphost/cache"
+GEN_DIR="$HOME/.jumphost/generator.d"
+
+for i in $(find $GEN_DIR -name '*.sh' -type f); do [ -x $i ] && $i; done
 
 grep --no-filename -E "^$1" $CACHE_DIR/*.findhost.tsv
 
